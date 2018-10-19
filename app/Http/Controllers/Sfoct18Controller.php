f72e6aaf527943d07bc93442f0eb0650c5d47a51
Joel Salgado <marvinjoel_25@hotmail.com>
2018-10-19 12:39:05 -0500

    segundo commit

---
+++ app/Http/Controllers/Sfoct18Controller.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Sfoct18;
+use Illuminate\Http\Request;
+
+class Sfoct18Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $model = Sfoct18::where('folio', $id)->first();
+
+        if($model){
+            $var = $model;
+        }else{
+            $var = 505;
+        }
+
+        return $var;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $model = Sfoct18::where('folio', $id)->first();
+
+        if($model){
+            $model->status = $request->status;
+            if($model->save()){
+                return 200;
+            }
+            else{
+                return 510;
+            }
+        }else{
+            return 520;
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Sfoct18.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sfoct18 extends Model
+{
+    protected $table = 'san_felipe_oct_18';
+    protected $primaryKey = 'folio';
+    public $timestamps = false;
+}
